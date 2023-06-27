b295051167025a20b0a83bff3d3ce1c44a70d3ec
IvanMartinez0 <132786345+IvanMartinez0@users.noreply.github.com>
2023-06-27 14:02:47 -0300

    comentarios
    comentarios acerca la base de datos

--- restaurante.sql
+++ restaurante.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE `mesa` (
+CREATE TABLE `mesa` ( /*creacion de la tabla mesa*/
   `id_mesa` int(11) primary key NOT NULL,
   `capacidad_mesa` varchar(15) NOT NULL,
   `num_mesa` varchar(5) NOT NULL,
   `tipo_mesa` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci; /*definicion del lenguaje de la tabla*/
 
-INSERT INTO `mesa` (`id_mesa`, `capacidad_mesa`, `num_mesa`,`tipo_mesa`) VALUES
+INSERT INTO `mesa` (`id_mesa`, `capacidad_mesa`, `num_mesa`,`tipo_mesa`) VALUES /* llenado de los campos */
 (1, '5 personas', '5','exterior'),
 (2, '5 personas', '6','exterior'),
 (3, '5 personas', '7','exterior'),
@@ -16,14 +16,14 @@ INSERT INTO `mesa` (`id_mesa`, `capacidad_mesa`, `num_mesa`,`tipo_mesa`) VALUES
 (7, '4 personas', '3','interior'),
 (8, '2 personas', '4','interior');
 
-CREATE TABLE `producto` (
+CREATE TABLE `producto` ( /*creacion de la tabla producto */
   `id_prod` int(11) primary key NOT NULL,
   `nombre_prod` varchar(50) NOT NULL,
   `precio_prod` varchar(30) NOT NULL,
   `descrip_prod` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci; /*declaracion del lenguaje de la tabla*/
 
-INSERT INTO `producto` ( `id_prod`, `nombre_prod`, `precio_prod`,`descrip_prod`) VALUES
+INSERT INTO `producto` ( `id_prod`, `nombre_prod`, `precio_prod`,`descrip_prod`) VALUES /*llenado de campos*/
 (1,'Sandwich de pollo a la parrilla ', '1400 pesos ','Sandwich de pollo cosinado en parrilla con pan de salvado y aderezos a eleccion'),
 (2,'Sandwich de jamon y queso ', '1160 pesos ','Sandwich de jamon y queso tostado'),
 (3,'Ensalada cesar', '950 pesos ','Ensalada cesar con lechuga romana y croûtons con jugo de limón, aceite de oliva, huevo, salsa Worcestershire, anchoas, ajo, mostaza de Dijon, queso parmesano y pimienta negra'),
@@ -35,22 +35,18 @@ INSERT INTO `producto` ( `id_prod`, `nombre_prod`, `precio_prod`,`descrip_prod`)
 (9,'Empanada arabe', '220 pesos ','Empanada arabe de carne molida con huevo, cebolla y jugo de limon'),
 (10,'Spaguetti a la bolognesa', '1500 pesos ','Fideos spaguetti con salsa de tomate y condimentada con ajo, sal, aceite de oliva, laurel, albaca y queso a gusto');
 
-CREATE TABLE `promo` (
+CREATE TABLE `promo` ( /*creacion de la tabla promo*/
   `id_promo` int(11) primary key NOT NULL,
   `nombre_promo` varchar(30) NOT NULL,
   `descrip_promo` text NOT NULL,
   `precio_promo` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci; /*definicion del lenguaje*/
 
-
-
-INSERT INTO `promo` (`id_promo`,`nombre_promo`, `descrip_promo`, `precio_promo`) VALUES
+INSERT INTO `promo` (`id_promo`,`nombre_promo`, `descrip_promo`, `precio_promo`) VALUES /*llenado de datos*/
 (1,'desayuno', 'cafe mediano + 2 criollos + una factura', '780 pesos'),
 (2,'almuerzo', 'milanesa de carne + papas hervidas + gaseosa a eleccion', '2400 pesos');
 
-
-
-CREATE TABLE `reserva` (
+CREATE TABLE `reserva` ( /*creacion de la tabla reserva*/
   `id_res` int(11) primary key NOT NULL,
   `nombre_res` varchar(30) NOT NULL,
   `dni_res` varchar(30) NOT NULL,
@@ -59,32 +55,29 @@ CREATE TABLE `reserva` (
   `mesa_reserva` int(11) NOT NULL,
   `apellido_res` varchar(30) NOT NULL,
   `tel_res` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci; /*definicion del lenguaje*/
 
 
-CREATE TABLE `restaurante` (
+CREATE TABLE `restaurante` ( /*creacion de la tabla restaurante*/
   `id_rest` int(11) primary key NOT NULL,
   `producto_rest` int(11) NOT NULL,
   `promo-rest` int(11) NOT NULL,
   `reserva_rest` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci; /*definicion del idioma*/
 
 
 ALTER TABLE `reserva`
- 
-  ADD KEY `mesa_reserva` (`mesa_reserva`);
-
+  ADD KEY `mesa_reserva` (`mesa_reserva`); /* definicion de la llave foranea*/
 
 ALTER TABLE `restaurante`
-
-  ADD KEY `producto_rest` (`producto_rest`),
+  ADD KEY `producto_rest` (`producto_rest`), /* definicion de llave foranea*/
   ADD KEY `promo-rest` (`promo-rest`),
   ADD KEY `reserva_rest` (`reserva_rest`);
 
-ALTER TABLE `reserva`
-  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`mesa_reserva`) REFERENCES `mesa` (`id_mesa`);
+ALTER TABLE `reserva` /*relaciones de la tabla reserva */
+  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`mesa_reserva`) REFERENCES `mesa` (`id_mesa`); 
 
-ALTER TABLE `restaurante`
+ALTER TABLE `restaurante` /*relaciones de la tabla restaurante*/
   ADD CONSTRAINT `restaurante_ibfk_1` FOREIGN KEY (`promo-rest`) REFERENCES `promo` (`id_promo`),
   ADD CONSTRAINT `restaurante_ibfk_2` FOREIGN KEY (`producto_rest`) REFERENCES `producto` (`id_prod`),
   ADD CONSTRAINT `restaurante_ibfk_3` FOREIGN KEY (`reserva_rest`) REFERENCES `reserva` (`id_res`);
