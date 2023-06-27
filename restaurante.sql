1516f1dcc7dc3218499818fa6c57350c00b28d27
IvanMartinez0 <132786345+IvanMartinez0@users.noreply.github.com>
2023-06-27 12:45:09 -0300

    Cargado de tablas
    Cargue las tablas 'mesa', 'producto'

--- restaurante.sql
+++ restaurante.sql
@@ -1,31 +1,44 @@
 
 CREATE TABLE `mesa` (
   `id_mesa` int(11) primary key NOT NULL,
-  `capacidad_mesa` varchar(30) NOT NULL,
-  `num_mesa` varchar(30) NOT NULL
+  `capacidad_mesa` varchar(15) NOT NULL,
+  `num_mesa` varchar(5) NOT NULL,
+  `tipo_mesa` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
-INSERT INTO `mesa` (`id_mesa`, `capacidad_mesa`, `num_mesa`) VALUES
-(1, '5', '1'),
-(2, '4', '2');
+INSERT INTO `mesa` (`id_mesa`, `capacidad_mesa`, `num_mesa`,`tipo_mesa`) VALUES
+(1, '5 personas', '5','exterior'),
+(2, '5 personas', '6','exterior'),
+(3, '5 personas', '7','exterior'),
+(4, '5 personas', '8','exterior'),
+(5, '4 personas', '2','interior'),
+(6, '4 personas', '1','interior'),
+(7, '4 personas', '3','interior'),
+(8, '2 personas', '4','interior');
 
 CREATE TABLE `producto` (
   `id_prod` int(11) primary key NOT NULL,
-  `nombre_prod` varchar(30) NOT NULL,
+  `nombre_prod` varchar(50) NOT NULL,
   `precio_prod` varchar(30) NOT NULL,
   `descrip_prod` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
-
-
-INSERT INTO `producto` ( `id_prod`, `nombre_prod`, `precio_prod`, `descrip_prod`) VALUES
-(1,'milanesa a la napolitana', '1100', 'milanesa de pollo a la napolitana'),
-(2,'papas fritas pequeñas', '350', 'paquete de papas fritas tamaño pequeño');
+INSERT INTO `producto` ( `id_prod`, `nombre_prod`, `precio_prod`,`descrip_prod`) VALUES
+(1,'Sandwich de pollo a la parrilla ', '1400','Sandwich de pollo cosinado en parrilla con pan de salvado y aderezos a eleccion'),
+(2,'Sandwich de jamon y queso ', '1160','Sandwich de jamon y queso tostado'),
+(3,'Ensalada cesar', '950','Ensalada cesar con lechuga romana y croûtons con jugo de limón, aceite de oliva, huevo, salsa Worcestershire, anchoas, ajo, mostaza de Dijon, queso parmesano y pimienta negra'),
+(4,'Ensalada de espinaca y fresas ', '1200','Ensalada de espinaca, fresas, nueces, queso de cabra, vinagre balsamico, miel, aceite de oliva extra virgen, sal y pimienta negra'),
+(5,'Tarta de frutilla ', '1100','Tarta de frutilla de harina de trigo, ralladura de limon, manteca, azucar, yema de huevo, crema de leche, frutilla y azucar impalpable, '),
+(6,'Tarta de durazno', '1250','Tarta de frutilla con harina de trigo, ralladura de limon, manteca, azucar, yema de huevo, crema de leche, duraznos y azucar impalpable '),
+(7,'Choripan ', '550','Choripan con chorizo porcino, pan flauta, aderezos a gusto y chimichurri'),
+(8,'Empanada de jamon y queso/carne', '200','Empanada de jamon cosido y queso fresco con huevo / carne molida condimentada con cebolla, pimienta y huevo'),
+(9,'Empanada arabe', '220','Empanada arabe de carne molida con huevo, cebolla y jugo de limon'),
+(10,'Spaguetti a la bolognesa', '1500','Fideos spaguetti con salsa de tomate y condimentada con ajo, sal, aceite de oliva, laurel, albaca y queso a gusto');
 
 CREATE TABLE `promo` (
   `id_promo` int(11) primary key NOT NULL,
   `nombre_promo` varchar(30) NOT NULL,
-  `descripcion_promo` text NOT NULL,
+  `descrip_promo` text NOT NULL,
   `precio_promo` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
