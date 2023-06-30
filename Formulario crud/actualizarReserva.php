d602d272846fd5d27baf6c35d522b16a097d84e9
IvanMartinez0 <132786345+IvanMartinez0@users.noreply.github.com>
2023-07-01 00:22:23 +0200

    Muestra correcta de campos
    ahora la pagina de actualizacion de reserva muestra correctamente los campos

--- Formulario crud/actualizarReserva.php
+++ Formulario crud/actualizarReserva.php
@@ -4,7 +4,11 @@
 
     $id_res=$_GET['id_res'];
 
-    $sql="SELECT * FROM reserva WHERE id_res='$id_res'";
+    $sql = "SELECT reserva.id_res, reserva.nombre_res, reserva.apellido_res, reserva.dni_res, reserva.tel_res, mesa.capacidad_mesa, horario.hora, reserva.fecha_res
+
+     FROM reserva JOIN horario ON reserva.id_res = horario.id_hora JOIN mesa ON reserva.id_res = mesa.id_mesa
+
+     WHERE reserva.id_res=$id_res";
     $query=mysqli_query($conexion, $sql);
 
     $row=mysqli_fetch_array($query);
@@ -27,10 +31,13 @@
                 <input type="text" name="Apellido" placeholder="Apellido" value="<?= $row['apellido_res']?>">
                 <input type="text" name="DNI" placeholder="DNI" value="<?= $row['dni_res']?>">
                 <input type="text" name="Telefono" placeholder="Telefono" value="<?= $row['tel_res']?>">
-                <input type="text" name="Mesa" placeholder="Mesa" value="<?= $row['mesa_res']?>">
-                <input type="text" name="Horario" placeholder="Horario" value="<?= $row['hora_res']?>">
+                <input type="text" name="Mesa" placeholder="Mesa" value="<?= $row['capacidad_mesa']?>">
+                <input type="text" name="Horario" placeholder="Horario" value="<?= $row['hora']?>">
                 <input type="date" name="Fecha" placeholder="fecha" value="<?= $row['fecha_res']?>">
 
+
+
+ ?>
                 <input type="submit" value="Actualizar">
             </form>
         </div>
