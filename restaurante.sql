e6ff3fd5902600798dd0ade1802cb54a486a415f
IvanMartinez0 <132786345+IvanMartinez0@users.noreply.github.com>
2023-07-01 04:35:04 +0200

    Cambios de ultimo momento

--- restaurante.sql
+++ restaurante.sql
@@ -1,4 +1,13 @@
 
+CREATE TABLE `menu` ( /*creacion de la tabla mesa*/
+  `id_menu` int(11) primary key NOT NULL AUTO_INCREMENT,
+  `tipo_menu` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO `menu` (`id_menu`, `tipo_menu`) VALUES /* llenado de los campos */
+(1, 'almuerzo'),
+(2, 'merienda');
+
 CREATE TABLE `mesa` ( /*creacion de la tabla mesa*/
   `id_mesa` int(11) primary key NOT NULL AUTO_INCREMENT,
   `capacidad_mesa` varchar(50) NOT NULL
@@ -29,24 +38,28 @@ CREATE TABLE `horario` ( /*creacion de la tabla horario*/
 (6, '12:30');
 
 
-CREATE TABLE `producto` ( /*creacion de la tabla producto */
+CREATE TABLE `producto` (
   `id_prod` int(11) primary key NOT NULL AUTO_INCREMENT,
   `nombre_prod` varchar(50) NOT NULL,
   `precio_prod` varchar(30) NOT NULL,
-  `descrip_prod` text NOT NULL
+  `descrip_prod` text NOT NULL,
+  `menu_prod` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci; /*declaracion del lenguaje de la tabla*/
 
-INSERT INTO `producto` ( `id_prod`, `nombre_prod`, `precio_prod`,`descrip_prod`) VALUES /*llenado de campos*/
-(1,'Sandwich de pollo a la parrilla ', '1400 pesos ','Sandwich de pollo cosinado en parrilla con pan de salvado y aderezos a eleccion'),
-(2,'Sandwich de jamon y queso ', '1160 pesos ','Sandwich de jamon y queso tostado'),
-(3,'Ensalada cesar', '950 pesos ','Ensalada cesar con lechuga romana y croûtons con jugo de limón, aceite de oliva, huevo, salsa Worcestershire, anchoas, ajo, mostaza de Dijon, queso parmesano y pimienta negra'),
-(4,'Ensalada de espinaca y fresas ', '1200 pesos ','Ensalada de espinaca, fresas, nueces, queso de cabra, vinagre balsamico, miel, aceite de oliva extra virgen, sal y pimienta negra'),
-(5,'Tarta de frutilla ', '1100 pesos ','Tarta de frutilla de harina de trigo, ralladura de limon, manteca, azucar, yema de huevo, crema de leche, frutilla y azucar impalpable, '),
-(6,'Tarta de durazno', '1250 pesos ','Tarta de frutilla con harina de trigo, ralladura de limon, manteca, azucar, yema de huevo, crema de leche, duraznos y azucar impalpable '),
-(7,'Choripan ', '550 pesos ','Choripan con chorizo porcino, pan flauta, aderezos a gusto y chimichurri'),
-(8,'Empanada de jamon y queso/carne', '200 pesos ','Empanada de jamon cosido y queso fresco con huevo / carne molida condimentada con cebolla, pimienta y huevo'),
-(9,'Empanada arabe', '220 pesos ','Empanada arabe de carne molida con huevo, cebolla y jugo de limon'),
-(10,'Spaguetti a la bolognesa', '1500 pesos ','Fideos spaguetti con salsa de tomate y condimentada con ajo, sal, aceite de oliva, laurel, albaca y queso a gusto');
+INSERT INTO `producto` (`id_prod`, `nombre_prod`, `precio_prod`, `descrip_prod`, `menu_prod`) VALUES
+(1, 'Sandwich de pollo a la parrilla', '1400 pesos', 'Sandwich de pollo cocinado en parrilla con pan de salvado y aderezos a elección', '1'),
+(2, 'Sandwich de jamon y queso', '1160 pesos', 'Sandwich de jamon y queso tostado', '1'),
+(3, 'Ensalada cesar', '950 pesos', 'Ensalada cesar con lechuga romana y croûtons con jugo de limón, aceite de oliva, huevo, salsa Worcestershire, anchoas, ajo, mostaza de Dijon, queso parmesano y pimienta negra', '1'),
+(4, 'Ensalada de espinaca y fresas', '1200 pesos', 'Ensalada de espinaca, fresas, nueces, queso de cabra, vinagre balsámico, miel, aceite de oliva extra virgen, sal y pimienta negra', '1'),
+(5, 'Tarta de frutilla', '1100 pesos', 'Tarta de frutilla de harina de trigo, ralladura de limón, manteca, azúcar, yema de huevo, crema de leche, frutilla y azúcar impalpable', '2'),
+(6, 'Tarta de durazno', '1250 pesos', 'Tarta de durazno con harina de trigo, ralladura de limón, manteca, azúcar, yema de huevo, crema de leche, duraznos y azúcar impalpable', '2'),
+(7, 'Choripan', '550 pesos', 'Choripan con chorizo porcino, pan flauta, aderezos a gusto y chimichurri', '1'),
+(8, 'Empanada de jamon y queso/carne', '200 pesos', 'Empanada de jamon y queso fresco con huevo / carne molida condimentada con cebolla, pimienta y huevo', '1'),
+(9, 'Empanada arabe', '220 pesos', 'Empanada arabe de carne molida con huevo, cebolla y jugo de limón', '1'),
+(10, 'Spaguetti a la bolognesa', '1500 pesos', 'Fideos spaguetti con salsa de tomate y condimentada con ajo, sal, aceite de oliva, laurel, albahaca y queso a gusto', '1'),
+(11, 'Cafe con leche grande', '550 pesos', 'Cafe con leche', '2'),
+(12, 'Batido de frutas Mix', '600 pesos', 'Batido de frutas tropicales combinado con leche y crema', '2');
+
 
 CREATE TABLE `promo` ( /*creacion de la tabla promo*/
   `id_promo` int(11) primary key NOT NULL AUTO_INCREMENT,
@@ -87,6 +100,8 @@ CREATE TABLE `restaurante` ( /*creacion de la tabla restaurante*/
   `reserva_rest` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci; /*definicion del idioma*/
 
+ALTER TABLE `menu`
+  ADD KEY `tipo_menu` (`tipo_menu`);
 
 ALTER TABLE `reserva`
   ADD KEY `mesa_res` (`mesa_res`), /* definicion de la llave foranea*/
@@ -101,8 +116,12 @@ ALTER TABLE `reserva` /*relaciones de la tabla reserva */
   ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`mesa_res`) REFERENCES `mesa` (`id_mesa`),
   ADD CONSTRAINT `reserva_ibfk_2` FOREIGN KEY (`hora_res`) REFERENCES `horario` (`id_hora`);
 
-ALTER TABLE `restaurante` /*relaciones de la tabla restaurante*/
-  ADD CONSTRAINT `restaurante_ibfk_1` FOREIGN KEY (`promo-rest`) REFERENCES `promo` (`id_promo`),
-  ADD CONSTRAINT `restaurante_ibfk_2` FOREIGN KEY (`producto_rest`) REFERENCES `producto` (`id_prod`),
-  ADD CONSTRAINT `restaurante_ibfk_3` FOREIGN KEY (`reserva_rest`) REFERENCES `reserva` (`id_res`);
+ALTER TABLE `producto`
+ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`menu_prod`) REFERENCES `menu` (`id_menu`);
+
+ALTER TABLE `restaurante`
+ADD CONSTRAINT `restaurante_ibfk_1` FOREIGN KEY (`promo-rest`) REFERENCES `promo` (`id_promo`),
+ADD CONSTRAINT `restaurante_ibfk_2` FOREIGN KEY (`producto_rest`) REFERENCES `producto` (`id_prod`),
+ADD CONSTRAINT `restaurante_ibfk_3` FOREIGN KEY (`reserva_rest`) REFERENCES `reserva` (`id_res`);
 COMMIT;
+
